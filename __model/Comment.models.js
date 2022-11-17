1aabd13fe746178f8760868a4bd77ac2476e1cac
malik016 <63663632+malik9153@users.noreply.github.com>
2022-11-17 09:52:47 +0000

    calibration complete

--- __model/Comment.models.js
+++ __model/Comment.models.js
@@ -6,7 +6,6 @@ return checkIDExists(review_id).then(() => {
 return db
 .query("SELECT * FROM comments WHERE review_id = $1;;", [review_id])
 .then((result) => {
-console.log(result.rows)
 return result.rows});
 })
 }
@@ -18,7 +17,6 @@ const queryValues =[body, username,review_id]
 const queryString = `INSERT INTO comments (body, author,review_id) VALUES ($1, $2, $3) RETURNING *;`
 return db 
 .query(queryString, queryValues).then ((result) => {
-console.log(result.rows)
 return result.rows[0]
 })
 
--- app.js
+++ app.js
@@ -21,7 +21,6 @@ next(err)
 })
 
 app.use((err,req,res,next) => {
-console.log(err)
 res.status(500).send({msg:'sever error!'})
 })
 
